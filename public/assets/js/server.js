23ef4fcd37d054e5ba732da3bd969a1d8fc412a4
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-04-09 23:09:45 -0400

    Added get api notes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -5,11 +5,39 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
 
-// GET route to return the notes.html file
+// GET route to return the notes.html file to user
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
   });
   
- 
+ // GET route to return the index.html file for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
+  
+  // GET route to read the db.json file and return all saved notes as JSON
+  app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+      } else {
+        const notes = JSON.parse(data);
+        res.json(notes);
+      }
+    });
+  });
+  
+  
+    
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+    
+    
+    
+  
